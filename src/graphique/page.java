25a00eec181e83f9eb91bff874c2d88ddaf6378d
KillianTurmorezoff <turmorek@3il.fr>
2024-03-11 09:08:35 +0100

    création projet et affichage d'une fenêtre vide

---
+++ src/graphique/page.java
@@ -0,0 +1,31 @@
+package graphique;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class page {
+
+    public static void main(String[] args) {
+        // Cette partie doit être exécutée dans l'EDT (Event Dispatch Thread)
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                creerEtAfficherFenetre();
+            }
+        });
+    }
+
+    public static void creerEtAfficherFenetre() {
+        // Création de la fenêtre
+        JFrame fenetre = new JFrame("Ma Fenêtre Swing");
+        
+        // Définition de la taille de la fenêtre
+        fenetre.setSize(400, 300);
+        
+        // Définition du comportement de fermeture par défaut
+        fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        // Rendre la fenêtre visible
+        fenetre.setVisible(true);
+    }
+}
+
