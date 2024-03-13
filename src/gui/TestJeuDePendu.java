1b3eb2ff04510490b598c178724e25eeeaacfa97
KillianTurmorezoff <turmorek@3il.fr>
2024-03-13 14:53:15 +0100

    ajout de quelques tests unitaires

---
+++ src/gui/TestJeuDePendu.java
@@ -0,0 +1,49 @@
+package gui;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import gui.ControlleurJeu;
+import gui.MotsReader;
+import gui.Partie;
+
+public class TestJeuDePendu {
+
+    @Test
+    public void testChoisirMotAleatoire() {
+        MotsReader motsReader = new MotsReader("mots.txt");
+        String mot = motsReader.choisirMotAleatoire();
+        assertNotNull(mot);
+        assertFalse(mot.isEmpty());
+    }
+
+    @Test
+    public void testDevineLettreCorrecte() {
+        Partie partie = new Partie("test");
+        assertTrue(partie.devinerLettre('t'));
+        assertEquals("t__t", partie.getMotActuel());
+    }
+
+    @Test
+    public void testDevineLettreIncorrecte() {
+        Partie partie = new Partie("test");
+        assertFalse(partie.devinerLettre('x'));
+        assertEquals("____", partie.getMotActuel());
+    }
+
+    @Test
+    public void testMotDevine() {
+        Partie partie = new Partie("test");
+        partie.devinerLettre('t');
+        partie.devinerLettre('e');
+        partie.devinerLettre('s');
+        assertTrue(partie.estMotDevine());
+    }
+
+    @Test
+    public void testMotNonDevine() {
+        Partie partie = new Partie("test");
+        partie.devinerLettre('t');
+        partie.devinerLettre('e');
+        assertFalse(partie.estMotDevine());
+    }
+}
